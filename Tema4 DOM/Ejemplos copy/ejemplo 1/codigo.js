dbca8332489c84f6db7e920fa413b9932ca1f8a5
tmartin1284 <profesor.tomasmr@ies-azarquiel.es>
2025-10-10 12:51:37 +0200

    ab

--- Tema4 DOM/Ejemplos copy/ejemplo 1/codigo.js
+++ Tema4 DOM/Ejemplos copy/ejemplo 1/codigo.js
@@ -47,61 +47,34 @@ if (oldLink) {
   document.head.removeChild(oldLink);
 }
 document.head.appendChild(link);
-let nuevo = null,
-  copia = null;
-let imagenes = document.getElementsByTagName("img");
-console.log("hay " + imagenes.length + " imagenes en la pagina");
-for (let i = 0; i < imagenes.length; i++) {
-  imagenes[i].src = "./img/" + personajes[i] + ".jpg";
-  imagenes[i].alt = UpperFirst(personajes[i]);
-  imagenes[i].title = UpperFirst(personajes[i]);
-  imagenes[i].className = "img-fluid";
 
-  let hermano = imagenes[i].nextElementSibling;
-  let primos = hermano.childNodes;
+const rowCard = document.getElementsByClassName("row")[0]; //de 0, poqeue nos devuelve un array
+let colCardModelo = rowCard.childNodes[0];
+console.log(rowCard.childNodes);
+i = 1;
+while (colCardModelo.nodeName != "DIV") colCardModelo = rowCard.childNodes[i++]; //cojo el primer div que me encuentre
 
-  //console.log(primos);
+console.log(colCardModelo);
 
-  for (let j = 0; j < primos.length; j++) {
-    if (primos[j].className === "card-title")
-      primos[j].textContent = UpperFirst(personajes[i]);
+while (rowCard.firstChild) {
+  rowCard.removeChild(rowCard.lastChild);
+}
+console.log(colCardModelo);
 
-    if (primos[j].tagName === "P") {
-      primos[j].innerHTML = `Esta es una imagen de ${UpperFirst(
-        personajes[i]
-      )}`;
-      primos[j].style.fontSize = "0.8em";
-      primos[j].style.color = "gray";
-      //nuevo = document.createElement("p");
-      copia = primos[j].cloneNode(true);
-      //hermano.appendChild(nuevo);
-      copia.textContent = "esta es una copia del parrafo anterior";
-      nuevo = document.createElement("p");
-      nuevo.innerHTML = "y este es un texto añadido con javascript";
-    }
-  }
-
-  if (nuevo) hermano.appendChild(nuevo);
-  if (copia) hermano.appendChild(copia);
+for (let personaje of personajes) {
+  let colCard = colCardModelo.cloneNode(true);
+  colCard.getElementsByTagName("img")[0].src = "./img/" + personaje + ".jpg";
+  colCard.getElementsByTagName("img")[0].alt = personaje;
+  colCard.getElementsByTagName("h4")[0].innerHTML = UpperFirst(personaje);
+  rowCard.appendChild(colCard);
 }
 
-/*
-en este ejeplo, hamos buscado las imagenes por tagName. Proque si hacemos un getElementsByClassTagName("div") nos devolvería demasiados
-otra opcción es recuperar los cards y luego buscar la imagen dentro de cada card 
-y lo podemos hacer con el className "card"
-*/
-
-let cards = document.getElementsByClassName("card");
-
-console.log(document.getElementsByTagName("img"));
-console.log(document.getElementsByTagName("p"));
-for (let card of cards) {
-  //cuidado con el for of el for in no funciona
-  //console.log(card.outerHTML);
-  console.log(card.getElementsByTagName("img"));
-  console.log(card.getElementsByTagName("p"));
-}
-
+let audio = document.createElement("audio");
+audio.setAttribute("src", "./img/musica.mp3");
+audio.setAttribute("loop", true);
+audio.setAttribute("controls", false);
+audio.setAttribute("autoplay", true);
+//document.getElementsByTagName("footer")[1].appendChild(audio);
 //=======================================================
 
 const a = document.createElement("a");
