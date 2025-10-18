eaaf37d828c4f2447a70bc75d37bcdfb700d5a18
tmartin1284 <profesor.tomasmr@ies-azarquiel.es>
2025-10-18 21:48:01 +0200

    de

--- Tema4 DOM/Ejemplos25/ejemplo2/codigo.js
+++ Tema4 DOM/Ejemplos25/ejemplo2/codigo.js
@@ -1,22 +1,17 @@
-
 /**
  * con esto leemos el archivo json. ahora mismo nos suena un poco raro.. usa dos promesas (asincronas etc)
  * de momento, sabed que esto funciona y ya esta
- * 
+ *
  * dentro de la función procesarJson, damos por supuesto que el archivo ya esta leido
  */
 
-
-fetch('./data/team.json')
-  .then(response => {
+fetch("./data/team.json")
+  .then((response) => {
     return response.json();
   })
-  .then(jsondata => procesarJSON(jsondata));
-
+  .then((jsondata) => procesarJSON(jsondata));
 
 function procesarJSON(jsondata) {
-
-
   /**
    * aqui empezamos a procesar el json 
    * y a crear los objetos que necesito para mostrar los datos
@@ -51,15 +46,13 @@ function procesarJSON(jsondata) {
   for (data in jsondata.teams) {
     let equipo = jsondata.teams[data];
 
-
     /**
-     * importante, a partir de ahora, vamos recuperando los elementos uno a uno usando el getElementbyID (todos tienen un id diferente), y mucho ojo, 
+     * importante, a partir de ahora, vamos recuperando los elementos uno a uno usando el getElementbyID (todos tienen un id diferente), y mucho ojo,
      * tendremos que cambiar el id a la hora de meterlo en el elemento equipo, porque si no, todos los equipos tendrian los mismos id...
      */
 
     let tarjeta = plantilla.cloneNode(true);
     contenedor.appendChild(tarjeta);
-
 
     tarjeta.setAttribute("id", "equipo_" + equipo.id);
     console.log(tarjeta.outerHTML);
@@ -68,62 +61,68 @@ function procesarJSON(jsondata) {
     propiedad.setAttribute("id", "imagen_" + equipo.id);
     propiedad.setAttribute("src", equipo.escudo);
 
-
     propiedad = document.getElementById("nombre");
     propiedad.setAttribute("id", "nombre_" + equipo.id);
-    propiedad.childNodes[0].nodeValue=equipo.nombre;
+    propiedad.childNodes[0].nodeValue = equipo.nombre;
     //propiedad.firstchild.nodeValue(equipo.nombre);  //y esto peta... manda cojones
 
     /**cambiamos los identificadores de los desplegables, de todos.
      * los nuevos identificadores, incluyen el id del equipo (del json)
      * y ya de paso, cambiamos los valores
-     * 
-    */
-   //con el video
-    propiedad=document.getElementById("mcvideo");
-    propiedad.setAttribute("id", "mcvideo_"+equipo.id)
-    propiedad=propiedad.getElementsByTagName("source")[0];
-    propiedad.setAttribute("src",equipo.video);
+     *
+     */
+    //con el video
+    propiedad = document.getElementById("mcvideo");
+    propiedad.setAttribute("id", "mcvideo_" + equipo.id);
+    propiedad = propiedad.getElementsByTagName("source")[0];
+    propiedad.setAttribute("src", equipo.video);
     //con el poster
-    propiedad=document.getElementById("mcposter");
-    propiedad.setAttribute("id", "mcposter_"+equipo.id);
-    propiedad=propiedad.getElementsByTagName("img")[0];
-    propiedad.setAttribute("src",equipo.poster);
+    propiedad = document.getElementById("mcposter");
+    propiedad.setAttribute("id", "mcposter_" + equipo.id);
+    propiedad = propiedad.getElementsByTagName("img")[0];
+    propiedad.setAttribute("src", equipo.poster);
     //con el entrenador
-    propiedad=document.getElementById("mcentrenador");
-        //hago lo mismo, pero un poco más comprimido
-    propiedad.setAttribute("id", "mcentrenador_"+equipo.id)
-    propiedad.getElementsByTagName("img")[0].setAttribute("src",equipo.imagenentrenador);
-    propiedad.getElementsByTagName("p")[0].childNodes[0].nodeValue=equipo.nombreentrenador;
+    propiedad = document.getElementById("mcentrenador");
+    //hago lo mismo, pero un poco más comprimido
+    propiedad.setAttribute("id", "mcentrenador_" + equipo.id);
+    propiedad
+      .getElementsByTagName("img")[0]
+      .setAttribute("src", equipo.imagenentrenador);
+    propiedad.getElementsByTagName("p")[0].childNodes[0].nodeValue =
+      equipo.nombreentrenador;
     //con el presidente
-    propiedad=document.getElementById("mcpresidente");
-    propiedad.setAttribute("id", "mcpresidente_"+equipo.id)
-    propiedad.getElementsByTagName("img")[0].setAttribute("src",equipo.imagenpresidente);
-    propiedad.getElementsByTagName("p")[0].childNodes[0].nodeValue=equipo.nombrepresidente;
-//con la ultima
-    propiedad=document.getElementById("mcmas");
-    propiedad.setAttribute("id", "mcmas_"+equipo.id)
-    propiedad.getElementsByTagName("p")[0].childNodes[0].nodeValue="El equipo tiene un presupuesto de "+equipo.presupuesto+" millones de Euros."
-    propiedad.getElementsByTagName("p")[1].childNodes[0].nodeValue="En la Liga Kings of Legends, el equipo usa la abreviatura " +equipo.abr;
+    propiedad = document.getElementById("mcpresidente");
+    propiedad.setAttribute("id", "mcpresidente_" + equipo.id);
+    propiedad
+      .getElementsByTagName("img")[0]
+      .setAttribute("src", equipo.imagenpresidente);
+    propiedad.getElementsByTagName("p")[0].childNodes[0].nodeValue =
+      equipo.nombrepresidente;
+    //con la ultima
+    propiedad = document.getElementById("mcmas");
+    propiedad.setAttribute("id", "mcmas_" + equipo.id);
+    propiedad.getElementsByTagName("p")[0].childNodes[0].nodeValue =
+      "El equipo tiene un presupuesto de " +
+      equipo.presupuesto +
+      " millones de Euros.";
+    propiedad.getElementsByTagName("p")[1].childNodes[0].nodeValue =
+      "En la Liga Kings of Legends, el equipo usa la abreviatura " + equipo.abr;
 
     /**
-     * y cambiamos los controladores, para que apunten al id del div que corresponden. 
+     * y cambiamos los controladores, para que apunten al id del div que corresponden.
      * los nuevos id son faciles, es el id del controlador (de la plantilla) más el id del equipo
      * y ponemos el color de los botones del color del equipo, por supuesto
      */
     for (boton of tarjeta.getElementsByTagName("button")) {
       boton.style.backgroundColor = equipo.color;
-      boton.setAttribute("data-bs-target",boton.getAttribute("data-bs-target")+"_"+equipo.id)
-      boton.setAttribute("aria-controls",boton.getAttribute("aria-controls")+"_"+equipo.id)
+      boton.setAttribute(
+        "data-bs-target",
+        boton.getAttribute("data-bs-target") + "_" + equipo.id
+      );
+      boton.setAttribute(
+        "aria-controls",
+        boton.getAttribute("aria-controls") + "_" + equipo.id
+      );
     }
-
-    
-
   }
-
-
-  const fecha=new Date ();
-  console.log(format(fecha,"full"));
-
-
 }
