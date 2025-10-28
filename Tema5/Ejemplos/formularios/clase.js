599c90569aae56d207011f0943699b6e4660b76c
tmartin1284 <profesor.tomasmr@ies-azarquiel.es>
2025-10-28 19:49:04 +0100

    aa

--- Tema5/Ejemplos/formularios/clase.js
+++ Tema5/Ejemplos/formularios/clase.js
@@ -1,37 +1,37 @@
-// Es una buena práctica que todo el código esté dentro del evento 'DOMContentLoaded' o 'load' 
-document.addEventListener('DOMContentLoaded', function () {
-  // Capturar todos los elementos 
-  const form = document.getElementById('contactForm');
-  const name = document.getElementById('name');
-  const email = document.getElementById('email');
-  const password = document.getElementById('password');
-  const passwordck = document.getElementById('passwordchek');
-  const acceptall = document.getElementById('Check1');
+// Es una buena práctica que todo el código esté dentro del evento 'DOMContentLoaded' o 'load'
+document.addEventListener("DOMContentLoaded", function () {
+  // Capturar todos los elementos
+  const form = document.getElementById("contactForm");
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const passwordck = document.getElementById("passwordchek");
+  const acceptall = document.getElementById("Check1");
 
   // Crear el manejador del evento para el envío del formulario ('submit')
-  form.addEventListener('submit', function (event) {
+  form.addEventListener("submit", function (event) {
     event.preventDefault(); // Evita que el formulario se envíe automáticamente
     event.stopPropagation(); // Evita que el evento se propague a elementos padre
 
-    // Llamar a la función principal de validación 
+    // Llamar a la función principal de validación
     if (validateForm()) {
       console.log("Todos los campos están bien, podemos proceder");
-      form.submit();  // Forzar el envío
+      form.submit(); // Forzar el envío
     } else {
-      console.log("Hay algún campo no válido. El usuario debe revisarlos.")
+      console.log("Hay algún campo no válido. El usuario debe revisarlos.");
     }
   });
 
-  // Esta función se encarga de validar todos los campos y 
+  // Esta función se encarga de validar todos los campos y
   // devolver un booleano: true si todos los campos están bien, false en caso contrario
   function validateForm() {
     // Esta bandera se inicializa como true.
     // En caso de encontrar un error en un campo, esta variable se convierte en false.
     var isValid = true;
-    console.log ("entra a validar");
+    console.log("entra a validar");
 
     // Lógica de validación personalizada
-    // Ejemplo de validación personalizada solo comprobando la longitud 
+    // Ejemplo de validación personalizada solo comprobando la longitud
     if (name.value.trim().length < 2) {
       markFieldAsNotValid(name);
       isValid = false;
@@ -39,31 +39,31 @@ document.addEventListener('DOMContentLoaded', function () {
       markFieldAsValid(name);
     }
 
-    // Podemos llamar a funciones personalizadas de validación de campos 
+    // Podemos llamar a funciones personalizadas de validación de campos
     // también podemos comprobar diferentes condiciones y mostrar diferentes mensajes de error
     if (email.value === "") {
       markFieldAsNotValid(email, "El correo electrónico es obligatorio");
       isValid = false;
     } else if (!isValidEmail(email.value)) {
-      markFieldAsNotValid(email, "Por favor, proporciona una dirección de correo válida.");
+      markFieldAsNotValid(
+        email,
+        "Por favor, proporciona una dirección de correo válida."
+      );
       isValid = false;
     } else {
       markFieldAsValid(email);
     }
 
-    if (password.value !== passwordck.value ) {
-            markFieldAsNotValid(passwordck);
-            markFieldAsNotValid(password);
+    if (password.value !== passwordck.value) {
+      markFieldAsNotValid(passwordck);
+      markFieldAsNotValid(password);
       isValid = false;
     } else {
-         markFieldAsValid(passwordck);
-         markFieldAsValid(password);
+      markFieldAsValid(passwordck);
+      markFieldAsValid(password);
     }
 
-
-
-
-    if ( !isValidPassword(password.value)) {
+    if (!isValidPassword(password.value)) {
       markFieldAsNotValid(password);
       markFieldAsNotValid(passwordck);
       isValid = false;
@@ -76,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function () {
       markFieldAsNotValid(acceptall, "debes marcar esta casilla si o si");
       isValid = false;
     } else {
-      markFieldAsValid(acceptall)
+      markFieldAsValid(acceptall);
     }
 
     // Después de validar todos los campos, devolvemos el valor de la bandera isValid
@@ -85,7 +85,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Definición de las funciones de validación de campos y otras funciones auxiliares
   // ....
-
 });
 
 function isValidEmail(email) {
@@ -94,7 +93,7 @@ function isValidEmail(email) {
 }
 
 function isValidPassword(passwd) {
-  return (passwd.length >= 6);
+  return passwd.length >= 6;
 }
 
 // Esta función marca un campo como no válido y añade el mensaje de error
