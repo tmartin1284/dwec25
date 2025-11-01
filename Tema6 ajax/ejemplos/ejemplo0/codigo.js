251a8ab400caaf5d733f9419c444cf9eeafdc52b
tmartin1284 <profesor.tomasmr@ies-azarquiel.es>
2025-11-01 19:40:55 +0100

    JSON

--- Tema6 ajax/ejemplos/ejemplo0/codigo.js
+++ Tema6 ajax/ejemplos/ejemplo0/codigo.js
@@ -1,27 +1,23 @@
-fetch('http://localhost:3000/posts')
-  .then(response => {
+fetch("https://pokeapi.co/api/v2/pokedex")
+  .then((response) => {
     return response.json();
   })
-  .then(jsondata => procesarJSON(jsondata))
-  .catch(e => { console.log(e) });
+  .then((jsondata) => {
+    procesarJSON(jsondata);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 function procesarJSON(jsondata) {
+  //mucho ojo, jsondata ya es una variable con el contenido json procesado. si intentamos procesarlo de nuevo, peta
+  const mostrar = document.getElementById("mostrar");
 
-     //mucho ojo, jsondata ya es una variable con el contenido json procesado. si intentamos procesarlo de nuevo, peta
-const mostrar=document.getElementById("mostrar");
-
-
-jsondata.forEach ( objeto => {
-     const p =document.createElement("p");
-     let texto="";
-     for (let prop in objeto) texto=texto+ prop +": " + objeto[prop]+ "  ";
-p.innerText=texto
-mostrar.appendChild(p)
-})
-
-
-
+  jsondata.results.forEach((objeto) => {
+    const p = document.createElement("p");
+    let texto = "";
+    for (let prop in objeto) texto = texto + prop + ": " + objeto[prop] + "  ";
+    p.innerText = texto;
+    mostrar.appendChild(p);
+  });
 }
-
-
-
